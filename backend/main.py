a73f7cbb2baeef6de26b30e4992810579d8a6724
ChronoFuture <chronofutureinstitute@outlook.com>
2026-03-27 17:37:57 -0500

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+kkfrom fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="ChronoFuture API")
+app = FastAPI()
+
+# 🔥 PERMITIR TODO (para pruebas)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
